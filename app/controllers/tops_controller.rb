02155f1a979e09d01695e0b0fd459ccda620ffab
miyu51 <thereishopeforatree.256261219@gmail.com>
2025-10-28 20:13:14 +0900

    細かな修正

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -4,10 +4,10 @@ class TopsController < ApplicationController
     # joinで検索対象に含める
     @q = Noshi.joins(:categories, :synonyms).ransack(params[:q])
     @column_q = Column.ransack(params[:q])
+    @use_cases = Category.where.not(use_case: [nil, '']).select(:use_case).distinct.order(:use_case)
     if params[:q].present?
       # includesで検索結果にも反映
       @noshis = @q.result(distinct: true).includes(:categories, :synonyms)
-      @use_cases = Category.where.not(use_case: [nil, '']).select(:use_case).distinct.order(:use_case)
       @columns = @column_q.result(distinct: true).includes(:tags)
       #キーワード検索判定
       keyword = :label_or_description_or_synonyms_word_or_categories_use_case_or_categories_religion_or_columns_title_or_columns_text_cont
@@ -16,7 +16,6 @@ class TopsController < ApplicationController
       @noshis = []
       @columns = []
       @keyword_search = false
-      @use_cases = []
     end
   end
 end
