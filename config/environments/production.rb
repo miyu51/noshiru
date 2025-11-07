47d0eb7e7558c84bc5fb8e7012d108adc4707583
miyu51 <thereishopeforatree.256261219@gmail.com>
2025-11-07 19:11:56 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,7 +108,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 465,
     domain:               'gmail.com',
     user_name:            ENV['SMTP_USERNAME'], # Gmailアドレス
     password:             ENV['SMTP_PASSWORD'], # App Password（2段階認証時）
