47142146f1b396edc282464d91d47a683ec8b283
miyu51 <thereishopeforatree.256261219@gmail.com>
2025-11-13 22:13:51 +0900

    修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import Rails from "@rails/ujs"
-Rails.start()
 
 document.addEventListener("turbo:load", function () {
   const menuBar = document.getElementById("menu-bar");
