5c33f0bf963c92a5c785056273b9d4304670734f
miyu51 <thereishopeforatree.256261219@gmail.com>
2025-11-15 15:02:33 +0900

    一旦保存

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
   include InlineSvg::ActionView::Helpers
+
+  def twitter_share_url(text:, url:, hashtags: [], via: nil)
+    base = "https://twitter.com/intent/tweet?"
+    params = {
+      text: text,
+      url: url,
+      hashtags: hashtags.join(","),
+      via: via
+    }.compact
+    base + params.to_query
+  end
 end
